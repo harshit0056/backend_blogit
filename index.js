1c2f7e0470a82aa1bc9d97d2bd14f4bb174b65de
harshit0056 <121183456+harshit0056@users.noreply.github.com>
2024-09-05 13:15:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,18 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',  // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Allow all standard methods
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Allow standard headers
+    credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
+}));
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
-
+app.get("/", (req, res) => {
+    res.json("Hello");
+});
 
 const PORT = process.env.PORT||8000;
 const username = process.env.DB_USERNAME;
